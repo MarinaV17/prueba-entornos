61a941ad2f0d4f7b2e5807cf86824ebb377f74a1
MarinaV17 <marina.vegaortega@educa.madrid.org>
2022-12-11 13:05:27 +0100

    commit desde VS Code

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hola desde Eclipse");
 		System.out.println("Hola desde IntelliJ");
-                                      System.out.println("Hola desde NetBeans");
+        System.out.println("Hola desde NetBeans");
+		System.out.println("Hola desde Visual Studio Code");
 	}
 }
